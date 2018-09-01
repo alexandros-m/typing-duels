51da192e0ddf3fa5500bf244bf951afcd0d24bd9
Alexandros Bartzos <alex@Alexandross-MacBook-Air.local>
2018-08-31 20:56:47 -0700

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,57 @@
+var socket = io();
+var mainRunning = false;
+var refreshIntervalId = null;
+
+//tells the server that a player has connected
+socket.emit('user_connected', 1);
+
+function focusOnTextBox() {
+	document.getElementById('textarea').focus();
+}
+
+function main(desiredString) {
+	var cps;
+	document.getElementById('textarea').value = '';
+	document.getElementById('typedString').value = '';
+	document.getElementById('desiredString').innerHTML = desiredString;
+	var timer = 0
+	var time = document.getElementById('timer');
+	var textInput = document.getElementById('textarea').value;
+	var counter = 0;
+	function checkIfCorrect() {
+		if (counter % 20 == 0) {
+			timer++;
+			if (timer > -1) {
+				time.innerHTML = 'Time elapsed: ' + String(timer);
+			}
+		}
+		counter++;
+		textInput = document.getElementById('textarea').value;
+		typedLetters = textInput.length;
+		if (textInput == desiredString.slice(0, typedLetters)) {
+			cps = Math.floor(typedLetters / timer);
+			document.getElementById('cps').innerHTML = "Characters per second: " + String(cps);
+			document.getElementById('typedString').innerHTML = textInput;
+		}
+		if (desiredString == textInput) {
+			clearInterval(refreshIntervalId);
+			socket.emit('user_finished', 1);
+			socket.on('win_or_lose', function(win_or_lose) {
+				time.innerHTML = "You " + win_or_lose;
+				time.innerHTML += '<br>CPS: ' + String(cps);
+				document.getElementById('cps').innerHTML = "";
+
+			});
+		}
+
+		document.getElementById('desiredString').innerHTML = desiredString;
+	}
+	refreshIntervalId = setInterval(checkIfCorrect, 50);
+}
+
+//when the server tells to, it starts a game
+socket.on('sentence', function (sentence) {
+	focusOnTextBox();
+	document.getElementById('user_status').innerHTML = '';
+	main(sentence);
+});
---
+++ server.js
@@ -0,0 +1,65 @@
+//TODO: when the tab closes, the game should reset.
+//TODO: the footer should be locked on the bottom of the page.
+//TODO: add icons (for example instead of 'time elapsed', i should have an icon)
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//loads the word_list from a file.
+var word_list = require('./public/js/wordList.js');
+word_list = word_list.word_list;
+
+var user_finished = 0;
+
+var port = process.env.PORT || 3000;
+
+//necessary to use linked css and javscript files on frontend
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+var user_count = 0;
+
+function createSentence() {
+    //creates a sentence
+    var sentenceLength = 10
+    var resultString = '';
+    for (var i = 0; i < sentenceLength; i++) {
+        wordIndex = Math.floor(Math.random() * 900);
+        resultString += word_list[wordIndex] + ' ';
+    }
+    var sliced = resultString.slice(0, -1) + '.';
+    return sliced;
+}
+
+function start_game() {
+    //tells the client to start a game
+    io.emit('sentence', createSentence());
+}
+
+io.on('connection', function(socket) {
+    user_count++;
+    socket.on('user_finished', function(trash) {
+        if (user_finished == 0){
+            socket.emit("win_or_lose", "win!");
+            user_finished = 1;
+        } else if (user_finished == 1) {
+            socket.emit("win_or_lose", "lose.");
+            user_finished = 0
+        }
+    });
+    if (user_count == 2) {
+        //if there are 2 users connected,
+        //the game starts and the counter
+        //gets initialized again, so other users
+        //can play too.
+        start_game();
+        user_count = 0;
+    }
+});
+
+http.listen(port, function(){});
